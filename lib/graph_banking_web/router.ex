03c95776f01030588e452321daba1b168149c424
ravi <rafael.vitor.r2@gmail.com>
2020-06-07 22:22:43 -0300

    Add graphiql route

--- lib/graph_banking_web/router.ex
+++ lib/graph_banking_web/router.ex
@@ -5,7 +5,12 @@ defmodule GraphBankingWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", GraphBankingWeb do
+  scope "/" do
     pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: GraphBankingWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: GraphBankingWeb.Endpoint}
   end
 end
